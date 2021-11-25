282671904a4164f23ddfb1298681eb7d2a3864f3
sunbo.kie <sunbo.kie@bytedance.com>
2021-11-25 14:32:46 +0800

    update the compilation script

--- script/build.sh
+++ script/build.sh
@@ -10,10 +10,8 @@ conan install .. --build=missing
 
 cmake -DCMAKE_BUILD_TYPE=Release -DSKIP_TEST=ON ..
 
-num_cpus=`grep 'physical id' /proc/cpuinfo | sort -u | wc -l`
+echo 'compiling starts with' ${nproc} 'cpus'
 
-echo 'compiling starts with' ${num_cpus} 'cpus'
-
-make -j ${num_cpus}
+make -j${nproc}
 
 make install
