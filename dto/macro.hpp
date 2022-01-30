a8b28e16aa4e5b901c093393f971db606d5cedf2
Kie <Kidsunbo@users.noreply.github.com>
2022-01-30 14:37:27 +0800

    Update macro.hpp
    Add two more helper macro.

--- dto/macro.hpp
+++ dto/macro.hpp
@@ -1,15 +1,17 @@
 #ifndef DTO_MACRO_HPP
 #define DTO_MACRO_HPP
 
-#define FROM_JSON_STR_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_string()? std::optional<std::string>(j.at(#name).get<std::string>()):std::nullopt
+#define FROM_JSON_STR_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_string()? std::make_optional(j.at(#name).get<std::string>()):std::nullopt
 
-#define FROM_JSON_INT_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_number_integer()? std::optional<int>(j.at(#name).get<int>()):std::nullopt
+#define FROM_JSON_INT_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_number_integer()? std::make_optional(j.at(#name).get<int>()):std::nullopt
 
-#define FROM_JSON_BOOL_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_boolean()? std::optional<bool>(j.at(#name).get<bool>()):std::nullopt
+#define FROM_JSON_BOOL_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_boolean()? std::make_optional(j.at(#name).get<bool>()):std::nullopt
 
-#define FROM_JSON_FLOAT_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_number_float()? std::optional<double>(j.at(#name).get<double>()):std::nullopt
+#define FROM_JSON_FLOAT_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_number_float()? std::make_optional(j.at(#name).get<double>()):std::nullopt
 
-#define FROM_JSON_NUM_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_number()? std::optional<long long>(j.at(#name).get<long long>()):std::nullopt
+#define FROM_JSON_NUM_OPT(j, u, name) u.name = j.contains(#name) && j[#name].is_number()? std::make_optional(j.at(#name).get<long long>()):std::nullopt
+
+#define FRON_JSON_TYPE_OPT(j, u, type, name) u.name = j.contains(#name) ? std::make_optional(j.at(#name).get<type>()) : std::nullopt
 
 #define FROM_JSON_STR(j, u, name, dv) u.name = j.contains(#name) && j[#name].is_string()? j.at(#name).get<std::string>():dv
 
@@ -21,18 +23,20 @@
 
 #define FROM_JSON_NUM(j, u, name, dv) u.name = j.contains(#name) && j[#name].is_number()? j.at(#name).get<long long>():dv
 
-#define FROM_JSON_VECTOR_INT(j, u, name) u.name = j.contains(#name) && j[#name].is_array()? j.at(#name).get<std::vector<std::int64_t>>():std::vector<std::int64_t>{};
+#define FROM_JSON_VECTOR_INT(j, u, name) u.name = j.contains(#name) && j[#name].is_array()? j.at(#name).get<std::vector<std::int64_t>>():std::vector<std::int64_t>{}
 
-#define FROM_JSON_VECTOR_STR(j, u, name) u.name = j.contains(#name) && j[#name].is_array()? j.at(#name).get<std::vector<std::string>>():std::vector<std::string>{};
+#define FROM_JSON_VECTOR_STR(j, u, name) u.name = j.contains(#name) && j[#name].is_array()? j.at(#name).get<std::vector<std::string>>():std::vector<std::string>{}
+
+#define FRON_JSON_TYPE(j, u, type, name) u.name = j.contains(#name) ? j.at(#name).get<type>() : type{}
 
 #define TO_JSON(j, u, name) j[#name] = u.name
 
 #define TO_JSON_OPT(j, u, name) if(u.name.has_value()) j[#name] = u.name.value()
 
-#define TO_JSON_VECTOR(j, u, name) if(!u.name.empty()) j[#name] = u.name;
+#define TO_JSON_VECTOR(j, u, name) if(!u.name.empty()) j[#name] = u.name
 
-#define TO_JSON_OPT_WITH_DEFAULT(j, u, name, dv) if(u.name.has_value()) j[#name] = u.name.value(); else j[#name] = dv;
+#define TO_JSON_OPT_WITH_DEFAULT(j, u, name, dv) if(u.name.has_value()) j[#name] = u.name.value(); else j[#name] = dv
 
-#define ADD_TO_MAP_OPT(m, u, name) if(u.name.has_value()) m[#name] = u.name;
+#define ADD_TO_MAP_OPT(m, u, name) if(u.name.has_value()) m[#name] = u.name
 
 #endif
